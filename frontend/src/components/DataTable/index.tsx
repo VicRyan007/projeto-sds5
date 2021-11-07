2964c37c87846872f319e03c34ad952987d53a62
Ryan Víctor <ryannautico306@gmail.com>
2021-11-06 21:48:42 -0300

    Pagination (atualization)

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -9,7 +9,7 @@ import Pagination from "components/Pagination";
 const DataTable = () => {
 
 
-    const[activePage,setActivePage] = useState()
+    const[activePage,setActivePage] = useState(0)
 
     const [page, setPage] = useState<SalePage>({
         first: true,
@@ -26,7 +26,7 @@ const DataTable = () => {
             })
     }, [activePage])
 
-    const changePage = (index:number) => {
+    const changePage = (index: number) => {
         setActivePage(index)
     }
 
